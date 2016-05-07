2dde64f959d5a09a323f14a9caec9a72a6d97ece
Paweł <pawelzaur@gmail.com>
2016-05-07 22:48:52 +0200

    Refactoring names

--- src/screen_settings.py
+++ src/screen_settings.py
@@ -6,16 +6,16 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
-# Set width and height of one cell in grid
+# Set width and height of cell in the grid (in pixels)
 CELL_WIDTH = 30
 CELL_HEIGHT = 30
 
-# Set number of rows and columns in grid
+# Set number of rows and columns in the grid
 NUM_ROWS = 20
 NUM_COLS = 30
 
-# Set margin between each cell
-MARGIN = 1
+# Set margin between each cell (in pixels)
+CELL_MARGIN = 1
 
-# Set the height and width of the screen
-SIZE = [CELL_WIDTH * NUM_COLS, CELL_HEIGHT * NUM_ROWS]
+# Set width and height of the screen
+SCREEN_SIZE = [CELL_WIDTH * NUM_COLS, CELL_HEIGHT * NUM_ROWS]

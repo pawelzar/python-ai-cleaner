9e709a4d57ba7fea10a738862f604f94d41cec9d
Paweł <pawelzaur@gmail.com>
2016-05-08 00:33:01 +0200

    Added docstrings

--- src/priority_queue.py
+++ src/priority_queue.py
@@ -6,10 +6,13 @@ class PriorityQueue:
         self.elements = []
 
     def empty(self):
+        """Return true if there are no elements in the queue. False otherwise."""
         return len(self.elements) == 0
 
     def put(self, item, priority):
+        """Add new item with priority to the queue. Item is placed in priority order."""
         heapq.heappush(self.elements, (priority, item))
 
     def get(self):
+        """Return coordinates of the point with smallest priority."""
         return heapq.heappop(self.elements)[1]
